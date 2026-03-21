dd64ae6ac8681b27c1d2b34e432c11699ecd02c2
Mukesh <mukeshtopcoder@gmail.com>
2026-03-21 13:21:23 +0530

    Updated

---
+++ Python-10AM-WE-Mar2026/Day5.py
@@ -0,0 +1,82 @@
+"""
+Python Tokens
+Variable , Constant
+    a = 10   ( a is assign to 10 )
+
+Python is a dynamic programming language
+int a = 23;
+char a = 'A';
+String st = "Aman";    # Static Programming Language's Variable
+
+a = 10
+a = 234.67
+a = 'Aman' # Dynamic Programming Language's Variable
+
+a = 10
+a = 23.67
+a = 'Aman'
+a = True
+print( type(a) )
+
+
+
+Data Types
+Numbers
+    - int (34,-678 , 0  etc) (non-decimal)
+    - float (34.67 , -345.78 , 0.0, 5.0 etc) decimal values
+    - binary number
+        0b1011101 , 0b1010011
+            a = 0b1001
+            a = 0b11001
+            a = 0b10101
+            print(a)
+    - Octal Number System
+        (0,1,2,3,4,5,6,7)
+        0o354 , 0o526 , 0o526 etc
+    - Hexadecimal Number System
+        (0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F)
+        0xABC , 0x76B
+        a = 0x15F
+        print(a)
+    - Complex Number
+        a+jb  => a - real , b - imaginary
+        3+8j , 5+j7
+        a = 2+8j
+        print(a)
+        print( a.real )
+        print( a.imag )
+String
+        'A' , "Aman" , "A" , 'Aman' , '''Aman'''
+        a = 'A'
+        a = 'Aman'
+        a = "Aman"
+        a = '''Aman'''
+        print( type(a) )  # all are 'str'
+
+        a = 'aman'
+        a = "aman is a good boy"
+        a = ''' aman is a good boy
+        he is my younger brother '''
+        print(a)
+Boolean
+        True
+        False
+Sequence Data Type
+    List , Tuple
+SET
+Dictionary
+
+    a = [34,56,67,78]  # List
+    a = (3,45,56,78,98) # Tuple
+    a = {23,56,67,89}  # Set
+    a = {1:34,3:67}   # Dictionary
+
+
+Built-in Functions
+    print , input , int , float  , type
+Operators
+    Arithmetic , relational , assignment , membership,
+    Identity , Logical , Bitwise
+Data Types:- Number, String , Bool , List,Tuple,Set,Dict
+
+"""
