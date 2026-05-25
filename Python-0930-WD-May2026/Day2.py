8c7e3da33586fb11dbf94e29396aa9332ac5cdf6
Mukesh <mukeshtopcoder@gmail.com>
2026-05-25 22:24:05 +0530

    Updated

---
+++ Python-0930-WD-May2026/Day2.py
@@ -0,0 +1,97 @@
+# Single Line Comment
+
+"""
+Multi
+Line
+Comment
+
+Built-in Functions
+    print (it print values on console screen(output screen))
+
+
+print('Hello India')
+print("Hello World")
+print('''hello india''')
+a = 10
+print('a')
+print(a)
+print("Hello a")
+print("Hello",a)
+a = 10
+b = 20
+print("value of a is",a,"and value of b is",b)
+print(f"value of a is {a} and value of b is {b}")
+
+
+
+a = 10
+b = 20
+c = a+b
+print("Addition :",c)
+
+
+    input (it is use to ask value from the user on console)
+
+a = input()
+b = input()
+c = a+b
+print("Addition :",c)
+
+    input always return a string/text value
+
+a = input("Enter A Number : ")
+b = input("Enter B Number : ")
+c = a+b   # concatenation (input return text)
+print("Addition :",c)
+
+
+    int (integer :- it is use to convert a value into numeric (do not support decimal/point value))
+
+a = int( input("Enter A Number : ") )
+b = int( input("Enter B Number : ") )
+c = a+b   
+print("Addition :",c)
+
+
+    float (it is use to convert a value into numeric (also supports decimal/point value))
+
+a = float( input("Enter A Number : ") )
+b = float( input("Enter B Number : ") )
+c = a+b   
+print("Addition :",c)
+
+
+PYTHON is a dynamic programming language
+int a = 10;    c/c++/Java (Static Language)
+a = 10 (Dynamic Language)
+
+
+a = 10
+print( type(a) )
+a = 23.56
+print( type(a) )
+a = 'A'
+print( type(a) )
+a = 3+8j
+print( type(a) )
+a = True
+print( type(a) )
+
+
+a = 10
+b = 234.34
+print(a+b)
+
+
+
+# WAP to calculate the gross salary of an employee where
+HRA is 20% and DA is 30% of his basic salary
+HINT:- Gross_Salary = Basic_Salary + HRA + DA
+
+bs = float(input("Enter Basic Salary : "))
+hra = bs*0.20
+da = bs*0.30
+gs = bs+hra+da
+print("Gross Salary :",gs)
+
+"""
