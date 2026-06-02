1007170d055508d066a1c14dc5ef622bc2c72dcf
Mukesh <mukeshtopcoder@gmail.com>
2026-06-02 14:16:25 +0530

    Updated

---
+++ Python-0930-WD-May2026/Day5.py
@@ -0,0 +1,58 @@
+"""
+Conditional Statements
+if, if_else, Nested if_else, elif, Complex Condition
+Looping:-    for , while
+
+if
+Syntax:-
+if condition:
+    True_Statements
+
+
+if 10>5:
+    print("Hello")
+
+if 10>50:
+    print("Hello")
+
+if 10>50:
+    print("Hello")
+print("World")
+
+
+if _ else
+Syntax:-
+if condition:
+    True_Statement
+else:
+    False_Statement
+
+if 10>50:
+    print("Hello")
+else:
+    print("Bye")
+
+
+if 10>5:
+    print("Hello")
+else:
+    print("Bye")
+
+
+# WAP to find greater value b/w two
+
+a = 100
+b = 200
+if a>b:
+    print(a)
+else:
+    print(b)
+
+
+"""
+
+
+
+
+
+
